074cae1df3775af3cd14a5a0451ea84e30dccb48
farnazii <ffarzipour92@gmail.com>
2023-03-04 02:07:13 +0330

    add pinia store

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './resources/router'
+import store from './resources/store'
 
-const app = createApp(App).use(router)
+const app = createApp(App).use(router).use(store)
 
 app.mount('#app')
