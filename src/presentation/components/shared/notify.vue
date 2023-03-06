8e0de25717dfd3dfd39dbaee3156dd1048adc81e
farnazii <ffarzipour92@gmail.com>
2023-03-07 01:06:49 +0330

    message handler

---
+++ src/presentation/components/shared/notify.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+export type messageProps = {
+  type: string
+  messageKey?: string | number
+  content: string
+  show?: boolean
+}
+
+const props = defineProps<messageProps>()
+</script>
+
+<template>
+  <div class="relative h-screen w-full">
+    <div
+      class="w-72 h-10 absolute left-1/2 translate-x-[-50%] flex items-center justify-center text-white rounded-lg transition-all top-[50px]"
+      :class="{
+        success: props.type === 'success',
+        error: props.type === 'error',
+      }"
+    >
+      <p>{{ props.content }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.error {
+  background-color: red;
+}
+.success {
+  background-color: green;
+}
+</style>
--- src/presentation/pages/Main.vue
+++ src/presentation/pages/Main.vue
@@ -6,14 +6,20 @@ import { useMainStore } from '../../resources/store/main.store'
 const appHeader = defineAsyncComponent(
   () => import('../../presentation/components/shared/Header.vue')
 )
-
+const notify = defineAsyncComponent(
+  () => import('../../presentation/components/shared/notify.vue')
+)
 const mainStore = useMainStore()
+console.log(mainStore)
 </script>
 <template>
   <div>
     <appHeader />
-    <!-- TODO: create message handler component -->
-    <p class="message-handler">{{ mainStore.message.content }}</p>
+    <notify
+      :type="mainStore.message.type"
+      :content="mainStore.message.content"
+      v-if="mainStore.message.show"
+    />
     <router-view />
   </div>
 </template>
